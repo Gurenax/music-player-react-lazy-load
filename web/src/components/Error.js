f72f37e8e6ed77e93b492740cac02cc13472d8b1
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-14 20:57:49 +1100

    Use Paper component for Error

--- web/src/components/Error.js
+++ web/src/components/Error.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
 
-const Error = ({
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+});
+
+function Error({
+  classes,
   error
-}) => (
-  <div>{ error.message }</div>
-)
+}) {
+  // const { classes } = props;
+  return (
+    <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography type="headline" component="h3">
+          Error
+        </Typography>
+        <Typography component="p">
+          { error.message }
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
 
-export default Error
+Error.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Error);
