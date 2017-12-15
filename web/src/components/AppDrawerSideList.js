0413fb23da813b495a8119794a090a64a6dd6e4a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-15 21:33:06 +1100

    Change menu icons

--- web/src/components/AppDrawerSideList.js
+++ web/src/components/AppDrawerSideList.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import ListSubheader from 'material-ui/List/ListSubheader';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Collapse from 'material-ui/transitions/Collapse';
-import InboxIcon from 'material-ui-icons/MoveToInbox';
-import DraftsIcon from 'material-ui-icons/Drafts';
-import SendIcon from 'material-ui-icons/Send';
-import ExpandLess from 'material-ui-icons/ExpandLess';
-import ExpandMore from 'material-ui-icons/ExpandMore';
-import StarBorder from 'material-ui-icons/StarBorder';
+// import InboxIcon from 'material-ui-icons/MoveToInbox';
+// import DraftsIcon from 'material-ui-icons/Drafts';
+// import SendIcon from 'material-ui-icons/Send';
+// import ExpandLess from 'material-ui-icons/ExpandLess';
+// import ExpandMore from 'material-ui-icons/ExpandMore';
+// import StarBorder from 'material-ui-icons/StarBorder';
+
+import MusicNoteIcon from 'material-ui-icons/MusicNote'
+import PlayCircleOutlineIcon from 'material-ui-icons/PlayCircleOutline'
+import LibraryMusicIcon from 'material-ui-icons/LibraryMusic'
+import CreateIcon from 'material-ui-icons/Create'
 
 const styles = theme => ({
   root: {
@@ -20,52 +26,84 @@ const styles = theme => ({
   nested: {
     paddingLeft: theme.spacing.unit * 4,
   },
+  link: {
+    textDecoration: 'none'
+  }
 });
 
-class AppDrawerSideList extends React.Component {
-  state = { open: true };
+const AppDrawerSideList = ({
+  classes
+}) => {
+  // state = { open: true };
 
-  handleClick = () => {
-    this.setState({ open: !this.state.open });
-  };
+  // handleClick = () => {
+  //   this.setState({ open: !this.state.open });
+  // };
 
-  render() {
-    const { classes } = this.props;
+  // const { classes } = this.props;
 
-    return (
-      <List className={classes.root} subheader={<ListSubheader>Nested List Items</ListSubheader>}>
+  return (
+    <List className={classes.root} subheader={<ListSubheader>Menu</ListSubheader>}>
+      
+      <Link className={classes.link} to='/artists'>
         <ListItem button>
           <ListItemIcon>
-            <SendIcon />
+            <MusicNoteIcon />
           </ListItemIcon>
-          <ListItemText inset primary="Sent mail" />
+          <ListItemText inset primary="Artists" />
         </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText inset primary="Drafts" />
-        </ListItem>
-        <ListItem button onClick={this.handleClick}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText inset primary="Inbox" />
-          {this.state.open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse component="li" in={this.state.open} timeout="auto" unmountOnExit>
-          <List disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText inset primary="Starred" />
-            </ListItem>
-          </List>
-        </Collapse>
+      </Link>
+      <List disablePadding>
+        <Link className={classes.link} to='/artists/new'>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <CreateIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="New" />
+          </ListItem>
+        </Link>
       </List>
-    );
-  }
+
+      <Link className={classes.link} to='/songs'>
+        <ListItem button>
+          <ListItemIcon>
+            <PlayCircleOutlineIcon />
+          </ListItemIcon>
+          <ListItemText inset primary="Songs" />
+        </ListItem>
+      </Link>
+      <List disablePadding>
+        <Link className={classes.link} to='/songs/new'>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <CreateIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="New" />
+          </ListItem>
+        </Link>
+      </List>
+      
+
+      <Link className={classes.link} to='/genres'>
+        <ListItem button>
+          <ListItemIcon>
+            <LibraryMusicIcon />
+          </ListItemIcon>
+          <ListItemText inset primary="Genres" />
+        </ListItem>
+      </Link>
+      <List disablePadding>
+        <Link className={classes.link} to='/genres/new'>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <CreateIcon />
+            </ListItemIcon>
+            <ListItemText inset primary="New" />
+          </ListItem>
+        </Link>
+      </List>
+    </List>
+  )
 }
 
 AppDrawerSideList.propTypes = {
