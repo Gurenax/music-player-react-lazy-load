880a2312674f600c974d273dad04fb7bcc65c8b4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-16 15:05:47 +1100

    Remove hooks as they are not needed after all

--- api/models/Song.js
+++ api/models/Song.js
@@ -9,24 +9,6 @@ const songSchema = new Schema({
   genres: [{ type: Schema.ObjectId, ref: 'Genre', default: [] }] // e.g. Pop
 })
 
-// songSchema.pre('save', function(next) { // For `this` to work: https://stackoverflow.com/questions/39166700/the-this-object-is-empty-in-presave/41212614#41212614
-//   console.log('PRE SAVE', this)  
-//   next()
-// })
-
-// songSchema.pre('remove', next => {
-//   console.log('PRE REMOVE')
-//   next()
-// })
-
-// songSchema.post('save', function(song) {
-//   console.log('POST SAVE: Song saved', song.artist, song._id)
-//   // Artist.update(
-//   //   { _id: song.artist },
-//   //   { $addToSet : { songs: song._id } }
-//   // ).exec()
-// })
-
 const Song = mongoose.model('Song', songSchema)
 
 module.exports = Song
